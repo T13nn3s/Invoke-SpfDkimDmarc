b34cb3e08baaf2f70b5471f1e609fbfc76775fc9
T13nn3s <t13nn3s@protonmail.com>
2021-07-13 00:06:23 +0200

    Update DomainHealthChecker.psm1
    Added:
    - Custom DKIM checker
    - Disabled Default DNS on 1.1.1.1
    - Bug fixing

--- DomainHealthChecker.psm1
+++ DomainHealthChecker.psm1
@@ -1,61 +1,24 @@
-
-<#PSScriptInfo
-
-.VERSION 1.3.1
-
-.GUID 829d6e7f-2d4c-40de-9ead-36e508258b89
-
-.AUTHOR Martien van Dijk
-
-.COMPANYNAME Avantage IT
-
-.COPYRIGHT (c) 2021 Martien van Dijk @ All rights reserved.
-
-.TAGS @('Windows','Email','Security','Email Security','SPF','DKIM','DMARC')
-
-.LICENSEURI https://github.com/T13nn3s/DomainHealthChecker/blob/main/LICENSE
-
-.PROJECTURI https://github.com/T13nn3s/Show-SpfDkimDmarc
-
-.ICONURI
-
-.EXTERNALMODULEDEPENDENCIES 
-
-.REQUIREDSCRIPTS
-
-.EXTERNALSCRIPTDEPENDENCIES
-
-.RELEASENOTES https://github.com/T13nn3s/Show-SpfDkimDmarc
-
-.PRIVATEDATA
-
+<#>
+HelpInfoURI 'https://github.com/T13nn3s/Show-SpfDkimDmarc/blob/main/README.md'
 #>
-
-<# 
-
-.DESCRIPTION 
- Is your email domain properly protected against abuse, such as email spoofing? This form of abuse can cause (image) damage to an organization. The PowerShell script DomainHealthChecker.ps1 checks the SPF, DKIM and DMARC record of one or more email domains and gives advice if necessary. 
-#> 
-
-Param()
 function Show-SpfDkimDmarc {
     [CmdletBinding()]
     param (
-        # Check a single domain
+        # Specifies the domain for resolving the SPF, DKIM and DMARC-record.
         [Parameter(
             Mandatory, ParameterSetName = 'domain',
             ValueFromPipeline = $True,
             ValueFromPipelineByPropertyName = $True,
-            HelpMessage = "Specify domain name whose SPF, DMARC and DKIM record should be checked.",
+            HelpMessage = "Specifies the domain for resolving the SPF, DKIM and DMARC-record.",
             Position = 1)]
         [string]$Name,
 
-        # Check domains from a file
+        # Show SPF, DKIM and DMARC-records from multiple domains from a file.
         [Parameter(
             Mandatory, ParameterSetName = 'file',
             ValueFromPipeline = $True,
             ValueFromPipelineByPropertyName = $True,
-            HelpMessage = "Specifies file name which contains a list of domain names.",
+            HelpMessage = "Show SPF, DKIM and DMARC-records from multiple domains from a file.",
             Position = 2)]
         [System.IO.FileInfo]$File,
 
@@ -67,8 +30,9 @@ function Show-SpfDkimDmarc {
 
         # DNS Server to use
         [Parameter(Mandatory = $false,
+            HelpMessage = "DNS Server to use.",
             Position = 4)]
-        [string]$Server = "1.1.1.1"
+        [string]$Server
     )
 
     begin {
@@ -104,6 +68,28 @@ function Show-SpfDkimDmarc {
                 $this.DkimAdvisory = $DkimAdvisory
             }
         }
+
+        if ($PSBoundParameters.ContainsKey('Server')) {
+            $SplatParameters = @{
+                'Server'      = $Server
+                'ErrorAction' = 'SilentlyContinue'
+            }
+        }
+        Else {
+            $SplatParameters = @{
+                'ErrorAction' = 'SilentlyContinue'
+            }
+        }
+
+        # Custom list of DKIM-selectors
+        # https://help.sendmarc.com/support/solutions/articles/44001891845-email-provider-commonly-used-dkim-selectors
+        $DkimSelectors = @(
+            'google', # Google
+            'everlytic', # Everlytic
+            'k1', # Mailchimp / Mandrill
+            'mxvault' # Global Micro
+            'dkim' # Hetzner
+        )
     }
 
     process {
@@ -113,13 +99,14 @@ function Show-SpfDkimDmarc {
                 return
             }
         }
-
+        
+       
         function StartDomainHealthCheck($domain) {
-
+          
             # Check SPF-record
-            $SPF = Resolve-DnsName -type TXT -name $Domain -server $Server -ErrorAction SilentlyContinue | where-object { $_.strings -match "v=spf1" } | Select-Object -ExpandProperty strings -ErrorAction SilentlyContinue
+            $SPF = Resolve-DnsName -Name $Domain -Type TXT @SplatParameters | where-object { $_.strings -match "v=spf1" } | Select-Object -ExpandProperty strings -ErrorAction SilentlyContinue
             if ($SPF -eq $null) {
-                $SpfAdvisory = "No SPF record found."
+                $SpfAdvisory = "Domain does not have an SPF record. To prevent abuse of this domain, please add an SPF record to it."
             }
             if ($SPF -is [array]) {
                 $SpfAdvisory = "Domain has more than one SPF-record. One SPF record for one domain. This is explicitly defined in RFC4408"
@@ -143,38 +130,45 @@ function Show-SpfDkimDmarc {
                     }
                 }
             }
-
+            
             # Check DKIM-record
             $DKIM = $null
             if ($DkimSelector) {
-                $DKIM = Resolve-DnsName -Type TXT -Name "$($DkimSelector)._domainkey.$($Domain)" -Server $Server -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Strings -ErrorAction SilentlyContinue
+                $DKIM = Resolve-DnsName -Type TXT -Name "$($DkimSelector)._domainkey.$($Domain)" @SplatParameters | Select-Object -ExpandProperty Strings -ErrorAction SilentlyContinue
                 if ($DKIM -like " ") {
                     $DkimAdvisory = "No DKIM-record found for selector $($DkimSelector)._domainkey."
                 }
                 elseif ($DKIM -match "v=DKIM1") {
                     $DkimAdvisory = "DKIM-record found."
-                    $DkimAdvisory
                 }
             }
-            Else {
-                $DkimSelector = "selector1" # Microsoft
-                $CnameSelector1 = Resolve-DnsName -Type CNAME -Name "$($DkimSelector)._domainkey.$Domain" -server $Server -ErrorAction SilentlyContinue
-                if ($CnameSelector1.Name -notmatch "domainkey") {
+            else {
+                # Microsoft default DKIM check
+                $DKIM = Resolve-DnsName -Type TXT -Name selector1._domainkey.$Domain @SplatParameters | Select-Object -ExpandProperty strings -ErrorAction SilentlyContinue
+                if ($DKIM -like " ") {
                     $DkimAdvisory = "We couldn't find a DKIM record associated with your domain."
                 }
-                else {
-                    $DKIM = Resolve-DnsName -Type TXT -Name $CnameSelector1.namehost -server $Server | Select-Object -ExpandProperty strings
-                    if ($DKIM -like "") {
+                elseif ($DKIM -match "v=DKIM1") {
+                    $DkimSelector = "selector1"
+                    $DkimAdvisory = "DKIM-record found."
+                } 
+            }
+            if ($DKIM -like " ") {
+                $DkimSelector = $null
+                # If DKIM is empty try different DKIM selectors
+                foreach ($DkimSelector in $DkimSelectors) {
+                    $DKIM = Resolve-DnsName -Type TXT -Name $DkimSelector._domainkey.$Domain @SplatParameters | Select-Object -ExpandProperty strings -ErrorAction SilentlyContinue
+                    if ($DKIM -eq $null) {
                         $DkimAdvisory = "We couldn't find a DKIM record associated with your domain."
                     }
                     elseif ($DKIM -match "v=DKIM1") {
-                        $DkimAdvisory = "DKIM-record is valid."
+                        $DkimAdvisory = "DKIM-record found."
                     } 
                 }
             }
         
             # Check DMARC-record
-            $DMARC = Resolve-DnsName -type TXT -name _dmarc.$Domain -Server $Server -ErrorAction SilentlyContinue | Select-Object -ExpandProperty strings -ErrorAction SilentlyContinue
+            $DMARC = Resolve-DnsName -type TXT -name _dmarc.$Domain @SplatParameters | Select-Object -ExpandProperty strings -ErrorAction SilentlyContinue
             if ($DMARC -like "") {
                 $DmarcAdvisory = "Does not have a DMARC record. This domain is at risk to being abused by phishers and spammers."
             }
