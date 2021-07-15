c03d1f61a85e21a5836c069d47af569331fbe00b
T13nn3s <t13nn3s@protonmail.com>
2021-07-15 17:36:38 +0200

    Working on splitting
    Working on splitting

--- DomainHealthChecker.psd1
+++ DomainHealthChecker.psd1
@@ -69,7 +69,7 @@ PowerShellVersion = '5.1'
 # NestedModules = @()
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-FunctionsToExport = 'Show-SpfDkimDmarc'
+FunctionsToExport = @('Show-SpfDkimDmarc','Get-SPFRecord','Get-DKIMRecord','Get-DMARCRecord')
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
 CmdletsToExport = '*'
---
+++ public/Get-DMARCRecord.ps1
@@ -0,0 +1,72 @@
+function Get-DMARCRecord {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory = $True,
+            ValueFromPipeline = $True,
+            ValueFromPipelineByPropertyName = $True,
+            HelpMessage = "Specifies the domain for resolving the DMARC-record."
+        )][Alias('Resolve-DMARCRecord')]
+        [string]$Name,
+
+        [Parameter(
+            ValueFromPipeline = $True,
+            ValueFromPipelineByPropertyName = $True,
+            HelpMessage = "Show DMARC-record from multiple domains from a file."
+        )][Alias('Path')]
+        [System.IO.FileInfo]$File,
+
+        [Parameter(Mandatory = $false,
+            HelpMessage = "DNS Server to use.")]
+        [string]$Server
+    )
+
+    begin {
+
+        if ($PSBoundParameters.ContainsKey('Server')) {
+            $SplatParameters = @{
+                'Type'        = 'TXT'
+                'Name'        = "_dmarc.$($Name)"
+                'Server'      = $Server
+                'ErrorAction' = 'SilentlyContinue'
+            }
+        }
+        Else {
+            $SplatParameters = @{
+                'Type'        = 'TXT'
+                'Name'        = "_dmarc.$($Name)"
+                'ErrorAction' = 'SilentlyContinue'
+            }
+        }
+
+        $DMARCObject = New-Object System.Collections.Generic.List[System.Object]
+    }
+
+    Process {
+        $DMARC = Resolve-DnsName @SplatParameters | Select-Object -ExpandProperty strings -ErrorAction SilentlyContinue
+        if ($DMARC -eq $null) {
+            $DmarcAdvisory = "Does not have a DMARC record. This domain is at risk to being abused by phishers and spammers."
+        }
+        Else {
+            switch -Regex ($DMARC) {
+                ('p=none') {
+                    $DmarcAdvisory = "Domain has a valid DMARC record but the DMARC (subdomain) policy does not prevent abuse of your domain by phishers and spammers."
+                }
+                ('p=quarantine') {
+                    $DmarcAdvisory = "Domain has a DMARC record and it is set to p=quarantine. To fully take advantage of DMARC, the policy should be set to p=reject."
+                }
+                ('p=reject') {
+                    $DmarcAdvisory = "Domain has a DMARC record and your DMARC policy will prevent abuse of your domain by phishers and spammers."
+                }
+            }
+        }
+    } 
+    end {
+        $DMARCReturnValues = New-Object psobject
+        $DMARCReturnValues | Add-Member NoteProperty "Name" $Name
+        $DMARCReturnValues | Add-Member NoteProperty "DmarcRecord" $DMARC
+        $DMARCReturnValues | Add-Member NoteProperty "DmarcAdvisory" $DmarcAdvisory
+        $DMARCObject.Add($DMARCReturnValues)
+        $DMARCReturnValues
+    }
+}
